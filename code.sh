9fbb54d4b190a2b7e5f40381aa97e6a77826a4da
hnp2022 <116392026+hnp2022@users.noreply.github.com>
2022-10-22 05:30:51 +0700

    Update code.sh

--- code.sh
+++ code.sh
@@ -13,7 +13,7 @@ gen64() {
 }
 install_3proxy() {
     echo "installing 3proxy"
-    URL="https://raw.githubusercontent.com/quayvlog/quayvlog/main/3proxy-3proxy-0.8.6.tar.gz"
+    URL="https://github.com/3proxy/3proxy/archive/refs/tags/0.9.4.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
     cd 3proxy-3proxy-0.8.6
     make -f Makefile.Linux
@@ -63,7 +63,7 @@ upload_proxy() {
 }
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "//$IP4/$port/$(gen64 $IP6)"
+        echo "duong/gnoud/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
